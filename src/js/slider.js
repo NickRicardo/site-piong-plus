780bd822926b27d652745241cc943dee270630e9
Nícolas Santos <nicollas.ricardo@hotmail.com>
2023-11-07 14:46:40 -0300

    adding slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,29 +1,18 @@
+const slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
-const slides = document.querySelectorAll('.carousel-slide');
-const slideCount = slides.length;
 
 function showSlide(slideIndex) {
-    if (slideIndex < 0) {
-        slideIndex = slideCount - 1;
-    } else if (slideIndex >= slideCount) {
-        slideIndex = 0;
-    }
-
-    slides.forEach((slide, index) => {
-        if (index === slideIndex) {
-            slide.style.transform = 'translateX(0)';
-        } else {
-            slide.style.transform = 'translateX(100%)';
-        }
-    });
-
+    slides[currentSlide].classList.remove('mostrar');
     currentSlide = slideIndex;
+    slides[currentSlide].classList.add('mostrar');
 }
 
 function nextSlide() {
-    showSlide(currentSlide + 1);
+    let nextIndex = currentSlide + 1;
+    if (nextIndex >= slides.length) {
+        nextIndex = 0;
+    }
+    showSlide(nextIndex);
 }
 
-setInterval(nextSlide, 5000);
-
-showSlide(currentSlide);
+setInterval(nextSlide, 2000);
