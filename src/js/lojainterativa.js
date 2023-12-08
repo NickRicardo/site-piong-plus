3a677535f608eff0895415638fc3e5c1b433e567
Nícolas Santos <nicollas.developer@gmail.com>
2023-12-08 16:00:20 -0300

    deu bug na loja

--- src/js/lojainterativa.js
+++ src/js/lojainterativa.js
@@ -1,38 +1,125 @@
 // Dados fictícios dos produtos
 const produtos = [
-    { nome: 'Avental Cirúrgico Proteção Total Impermeável', categoria: 'categoria1', imagem: '../img/produtos/categorias/aventaisMacacao/avental1.png', especificacoes: 'Usado como EPI em proteção do usuário em diversos procedimentos' },
+    {
+        nome: 'Avental Cirúrgico Proteção Total Impermeável',
+        categoria: 'categoria1',
+        imagem: '../img/produtos/categorias/aventaisMacacao/aventalProtecao.png',
+        especificacoes: 'Usado como EPI em proteção do usuário em diversos procedimentos',
+        paginaDetalhes: '../html/produtos/aventalProtecao.html'
+    },
 
-    { nome: 'Avental Emergency Plus | LC Impermeável', categoria: 'categoria1', imagem: '../img/produtos/categorias/aventaisMacacao/aventalEmergency.png', especificacoes: 'Usado como EPI em proteção do usuário em diversos procedimentos' },
+    {
+        nome: 'Avental Emergency Plus | LC Impermeável',
+        categoria: 'categoria1',
+        imagem: '../img/produtos/categorias/aventaisMacacao/aventalEmergency.png',
+        especificacoes: 'Usado como EPI em proteção do usuário em diversos procedimentos'
+    },
 
-    { nome: 'Calça e Blusa | Pijama SSMMS', categoria: 'categoria2', imagem: '../img/produtos/categorias/calcaBlusa/calcaeblusa1.png', especificacoes: 'Usada em procedimentos de consulta médica, exames, emergência,  visita a pacientes etc.' },
 
-    { nome: 'CAMISOLA AZUL | SSMMS', categoria: 'categoria3', imagem: '../img/produtos/categorias/camisolaBatas/camisola1.png', especificacoes: 'Usada em procedimentos de consulta médica, exames,  emergência,  visita a pacientes etc' },
+    {
+        nome: 'Calça e Blusa | Pijama SSMMS',
+        categoria: 'categoria2',
+        imagem: '../img/produtos/categorias/calcaBlusa/calcaeblusa1.png',
+        especificacoes: 'Usada em procedimentos de consulta médica, exames, emergência,  visita a pacientes etc.'
+    },
 
-    { nome: 'CAMPO CIRÚRGICO | SSMMS ', categoria: 'categoria4', imagem: '../img/produtos/categorias/campoCirurgico/campoCirurgico1.png', especificacoes: 'Indicado para procedimentos onde seja necessária   a  manutenção  da   técnica asséptica  e  o  isolamento  da área á ser tratada' },
+    {
+        nome: 'CAMISOLA AZUL | SSMMS',
+        categoria: 'categoria3',
+        imagem: '../img/produtos/categorias/camisolaBatas/camisola1.png',
+        especificacoes: 'Usada em procedimentos de consulta médica, exames,  emergência,  visita a pacientes etc'
+    },
 
-    { nome: 'CAMPO CIRÚRGICO COM FENESTRA | SSMMS', categoria: 'categoria5', imagem: '../img/produtos/categorias/campoCirurgico/campoCirurgico2.png', especificacoes: 'Indicado para procedimentos onde seja necessária   amanutenção   da    técnica asséptica  e  o  isolamento  da  área  ser tratada.' },
+    {
+        nome: 'CAMPO CIRÚRGICO | SSMMS ',
+        categoria: 'categoria4',
+        imagem: '../img/produtos/categorias/campoCirurgico/campoCirurgico1.png',
+        especificacoes: 'Indicado para procedimentos onde seja necessária   a  manutenção  da   técnica asséptica  e  o  isolamento  da área á ser tratada'
+    },
 
-    { nome: 'CAMPO CIR. OFTÁLMICO | SSMMS ', categoria: 'categoria5', imagem: '../img/produtos/categorias/campoOftalmico/campoOftalmico.png', especificacoes: 'Indicado para procedimentos onde seja necessária   a  manutenção  da   técnica asséptica  e  o  isolamentoda área á ser tratada.' },
+    {
+        nome: 'CAMPO CIRÚRGICO COM FENESTRA | SSMMS',
+        categoria: 'categoria5',
+        imagem: '../img/produtos/categorias/campoCirurgico/campoCirurgico2.png',
+        especificacoes: 'Indicado para procedimentos onde seja necessária   amanutenção   da    técnica asséptica  e  o  isolamento  da  área  ser tratada.'
+    },
 
-    { nome: 'COBERTURA IMPERMEÁVEL PARA MESA MAYO | LC 50g', categoria: 'categoria6', imagem: '../img/produtos/categorias/coberturaMesa/cobertura1.png', especificacoes: 'Indicado  para cobertura  da mesa de instrumentos com impermeabilidade e reforço absorvente.' },
+    {
+        nome: 'CAMPO CIR. OFTÁLMICO | SSMMS ',
+        categoria: 'categoria5',
+        imagem: '../img/produtos/categorias/campoOftalmico/campoOftalmico.png',
+        especificacoes: 'Indicado para procedimentos onde seja necessária   a  manutenção  da   técnica asséptica  e  o  isolamentoda área á ser tratada.'
+    },
 
-    { nome: 'COBERTURA IMPERMEÁVEL PLÁTICO PBD', categoria: 'categoria6', imagem: '../img/produtos/categorias/coberturaMesa/cobertura2.png', especificacoes: 'Indicado  para cobertura  da mesa de instrumentos com impermeabilidade e reforço absorvente.' },
+    {
+        nome: 'COBERTURA IMPERMEÁVEL PARA MESA MAYO | LC 50g',
+        categoria: 'categoria6',
+        imagem: '../img/produtos/categorias/coberturaMesa/cobertura1.png',
+        especificacoes: 'Indicado  para cobertura  da mesa de instrumentos com impermeabilidade e reforço absorvente.'
+    },
 
-    { nome: 'INVÓLUCRO/WRAP', categoria: 'categoria7', imagem: '../img/produtos/categorias/involucroWrap/involucro1.png', especificacoes: 'Indicado como invólucro para proteger instrumentais cirúrgicos e materiais no processo de esterilização a vapor, óxido de etileno  ou  plasma  de  peróxido de hidrogênio' },
+    {
+        nome: 'COBERTURA IMPERMEÁVEL PLÁTICO PBD',
+        categoria: 'categoria6',
+        imagem: '../img/produtos/categorias/coberturaMesa/cobertura2.png',
+        especificacoes: 'Indicado  para cobertura  da mesa de instrumentos com impermeabilidade e reforço absorvente.'
+    },
 
-    { nome: 'ORTOPEDIA E TRAUMATOLOGIA', categoria: 'categoria8', imagem: '../img/produtos/categorias/kitCirurgico/kitCirurgico1.png', especificacoes: 'Kit Cirúrgico Ortopédico Ombro CÓD. 49.82.69 ANVISA: 803.420.60.005' },
+    {
+        nome: 'INVÓLUCRO/WRAP',
+        categoria: 'categoria7',
+        imagem: '../img/produtos/categorias/involucroWrap/involucro1.png',
+        especificacoes: 'Indicado como invólucro para proteger instrumentais cirúrgicos e materiais no processo de esterilização a vapor, óxido de etileno  ou  plasma  de  peróxido de hidrogênio'
+    },
 
-    { nome: 'CIRUGIAS GERAIS / UNIVERSAIS', categoria: 'categoria8', imagem: '../img/produtos/categorias/kitCirurgico/kitCirurgico.png', especificacoes: 'Kit Cirúrgico Ortopédico Ombro CÓD. 49.82.69 ANVISA: 803.420.60.005' },
+    {
+        nome: 'ORTOPEDIA E TRAUMATOLOGIA',
+        categoria: 'categoria8',
+        imagem: '../img/produtos/categorias/kitCirurgico/kitCirurgico1.png',
+        especificacoes: 'Kit Cirúrgico Ortopédico Ombro CÓD. 49.82.69 ANVISA: 803.420.60.005'
+    },
 
-    { nome: 'CIRUGIAS GERAIS / UNIVERSAIS', categoria: 'categoria8', imagem: '../img/produtos/categorias/kitCirurgico/kitCirurgico2.png', especificacoes: 'Kit Cirúrgico Ortopédico Ombro CÓD. 49.82.69 ANVISA: 803.420.60.005' },
+    {
+        nome: 'CIRUGIAS GERAIS / UNIVERSAIS',
+        categoria: 'categoria8',
+        imagem: '../img/produtos/categorias/kitCirurgico/kitCirurgico.png',
+        especificacoes: 'Kit Cirúrgico Ortopédico Ombro CÓD. 49.82.69 ANVISA: 803.420.60.005'
+    },
 
-    { nome: 'CIRUGIAS GERAIS / UNIVERSAIS', categoria: 'categoria8', imagem: '../img/produtos/categorias/kitCirurgico/kitCirurgico3.png', especificacoes: 'Kit Cirúrgico Ortopédico Ombro CÓD. 49.82.69 ANVISA: 803.420.60.005' },
+    {
+        nome: 'CIRUGIAS GERAIS / UNIVERSAIS',
+        categoria: 'categoria8',
+        imagem: '../img/produtos/categorias/kitCirurgico/kitCirurgico2.png',
+        especificacoes: 'Kit Cirúrgico Ortopédico Ombro CÓD. 49.82.69 ANVISA: 803.420.60.005'
+    },
 
-    { nome: 'CIRUGIAS GERAIS / UNIVERSAIS', categoria: 'categoria9', imagem: '../img/produtos/categorias/kitOdonto/kitCirurgico4.png', especificacoes: 'Kit Cirúrgico Ortopédico Ombro CÓD. 49.82.69 ANVISA: 803.420.60.005' },
+    {
+        nome: 'CIRUGIAS GERAIS / UNIVERSAIS',
+        categoria: 'categoria8',
+        imagem: '../img/produtos/categorias/kitCirurgico/kitCirurgico3.png',
+        especificacoes: 'Kit Cirúrgico Ortopédico Ombro CÓD. 49.82.69 ANVISA: 803.420.60.005'
+    },
 
-    { nome: 'CIRUGIAS GERAIS / UNIVERSAIS', categoria: 'categoria8', imagem: '../img/produtos/categorias/kitCirurgico/kitCirurgico2.png', especificacoes: 'Kit Cirúrgico Ortopédico Ombro CÓD. 49.82.69 ANVISA: 803.420.60.005' },
+    {
+        nome: 'CIRUGIAS GERAIS / UNIVERSAIS',
+        categoria: 'categoria9'
+        , imagem: '../img/produtos/categorias/kitOdonto/kitCirurgico4.png',
+        especificacoes: 'Kit Cirúrgico Ortopédico Ombro CÓD. 49.82.69 ANVISA: 803.420.60.005'
+    },
 
-    { nome: 'CIRUGIAS GERAIS / UNIVERSAIS', categoria: 'categoria8', imagem: '../img/produtos/categorias/kitCirurgico/kitCirurgico3.png', especificacoes: 'Kit Cirúrgico Ortopédico Ombro CÓD. 49.82.69 ANVISA: 803.420.60.005' }
+    {
+        nome: 'CIRUGIAS GERAIS / UNIVERSAIS',
+        categoria: 'categoria8',
+        imagem: '../img/produtos/categorias/kitCirurgico/kitCirurgico2.png',
+        especificacoes: 'Kit Cirúrgico Ortopédico Ombro CÓD. 49.82.69 ANVISA: 803.420.60.005'
+    },
+
+    {
+        nome: 'CIRUGIAS GERAIS / UNIVERSAIS',
+        categoria: 'categoria8',
+        imagem: '../img/produtos/categorias/kitCirurgico/kitCirurgico3.png',
+        especificacoes: 'Kit Cirúrgico Ortopédico Ombro CÓD. 49.82.69 ANVISA: 803.420.60.005'
+    }
 ];
 
 // Lista para armazenar as categorias selecionadas
@@ -73,8 +160,9 @@ function exibirProdutos(produtosParaExibir) {
 
 function exibirDetalhes(indiceProduto) {
     const produtoSelecionado = produtos[indiceProduto];
-    window.location.href = `../html/produtos/aventaisMacacoes.html?produto=${encodeURIComponent(produtoSelecionado.nome)}`;
+    window.location.href = `../html/produtos/${produtoSelecionado.paginaDetalhes}?produto=${encodeURIComponent(produtoSelecionado.nome)}`;
 }
+
 
 function exibirProdutos(produtosParaExibir) {
     const produtosContainer = document.getElementById('produto');
@@ -91,7 +179,6 @@ function exibirProdutos(produtosParaExibir) {
         produtosContainer.innerHTML += produtoHTML;
     });
 }
-
 
 // Chamada inicial para exibir todos os produtos
 filtrarProdutos();
