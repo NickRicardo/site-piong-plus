4a02d60665d80e1f310a411ed7c235e7b4718b61
Nícolas Santos <nicollas.developer@gmail.com>
2024-01-16 09:25:16 -0300

    validação de CPF concluida

--- src/js/form.js
+++ src/js/form.js
@@ -1,3 +1,21 @@
+// Adicionando o evento de envio do formulário
+document.getElementById('resumeForm').addEventListener('submit', function (event) {
+    // Obtendo o valor do CPF
+    var cpfInput = document.getElementById('cpf');
+    var cpf = cpfInput.value;
+
+    // Verificando se o CPF já foi utilizado
+    if (localStorage.getItem(cpf)) {
+        alert('Você já enviou um currículo com este CPF. Não é permitido enviar mais de um currículo.');
+        event.preventDefault(); // Impede o envio do formulário
+        return;
+    }
+
+    // Salvando o CPF no localStorage
+    localStorage.setItem(cpf, 'enviado');
+});
+
+// Função para exibir o nome do arquivo
 function displayFileName() {
     var fileInput = document.getElementById('resume');
     var fileNameInfo = document.getElementById('file-name-info');
