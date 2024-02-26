2a991c905a55818335f9c707b65be7abf92c090f
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-02-26 16:03:34 -0300

    Adicionado banner de cookies funcionando

--- src/js/cookies.js
+++ src/js/cookies.js
@@ -1,19 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const cookieBanner = document.getElementById("cookie-banner");
+var msgCookies = document.querySelector('.cookies-msg');
 
-    // Verifica se o usuário já aceitou os cookies
-    const cookiesAccepted = localStorage.getItem("cookies-accepted");
+function aceito(){
+    localStorage.lgpd = 'sim'
+    msgCookies.classList.remove('mostrarCookie')
+}
 
-    if (!cookiesAccepted) {
-        // Mostra o banner de cookies se ainda não foi aceito
-        cookieBanner.style.display = "block";
-    }
-
-    // Adiciona evento de clique ao botão de aceitar cookies
-    document.getElementById("accept-cookies").addEventListener("click", function() {
-        // Esconde o banner de cookies
-        cookieBanner.style.display = "none";
-        // Armazena a preferência de consentimento do usuário localmente
-        localStorage.setItem("cookies-accepted", true);
-    });
-});
+if(localStorage.lgpd == 'sim'){
+    msgCookies.classList.remove('mostrarCookie')
+}else{
+    msgCookies.classList.add('mostrarCookie')
+}
